f16e9141d753902b43f5a6e78c141a354286dab0
Gillinghammer <colingillingham@gmail.com>
2017-04-28 19:08:02 +0100

    created instagram authorization successfully

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
   config = require('./config/config'),
   glob = require('glob'),
-  mongoose = require('mongoose');
+  mongoose = require('mongoose'),
+  api = require('instagram-node').instagram();
 
 mongoose.connect(config.db);
 var db = mongoose.connection;
@@ -13,7 +14,34 @@ var models = glob.sync(config.root + '/app/models/*.js');
 models.forEach(function (model) {
   require(model);
 });
+
 var app = express();
+
+
+api.use({
+  client_id: 'f354d0ea817643559a11a06a7c905264',
+  client_secret: 'cee7bf9981bc438d8b0a8dc168a1cbae'
+});
+
+var redirect_uri = 'http://localhost:3000/handleauth';
+
+exports.authorize_user = function(req, res) {
+  res.redirect( api.get_authorization_url( redirect_uri, { scope: ['likes','basic','relationships'] } ) );
+};
+
+exports.handleauth = function(req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function(err, result){
+    if (err) {
+      console.log(err.body);
+    } else {
+      console.log('Yay! Access token is ' + result.access_token);
+      res.send('You made it!!');
+    }
+  });
+};
+
+app.get('/authorize_user', exports.authorize_user);
+app.get('/handleauth', exports.handleauth);
 
 module.exports = require('./config/express')(app, config);
 
